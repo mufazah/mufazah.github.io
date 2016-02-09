4a37d1126c13ba574fb2e222fd23648df32bcdb1
Lula <lulabarneche@Lulas-MacBook-Air.local>
2016-02-09 09:58:45 -0500

    java

--- javascript/font.js
+++ javascript/font.js
@@ -1,4 +1,19 @@
-function myFunction() {
-    var x = document.getElementById("afont");          
-    x.style.color = "red"; 
+status = 1;
+function changeStyle() {
+//Note the lowercase first letter.
+x = document.getElementById("bodtext");
+
+if(status==1) {
+    x.style.color = 'blue';
+    status = 2;
+}
+else if(status==2) {
+    x.style.color = 'red';
+    status = 3;
+}
+else if(status==3) {
+    x.style.color = 'yellow';
+    status = 1;
+}
+
 }
